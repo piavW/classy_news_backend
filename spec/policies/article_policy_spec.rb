c7ad467b54e7c51ede272e773995e60922195271
Becca <beccaburns89@gmail.com>
2019-10-31 12:05:19 +0100

    Updates policy spec for subscriber & journalist.

--- spec/policies/article_policy_spec.rb
+++ spec/policies/article_policy_spec.rb
@@ -1,30 +1,34 @@
 describe ArticlePolicy do
   subject { described_class.new(user, article) }
-
   let(:article) { create(:article) }
 
   context 'user is a subscriber' do
-    let(:user) { create(:user, role: 'subscriber') }
-
+    let(:user) { nil }
     it { is_expected.to permit_actions [:show, :index] }
+  end
+
+  context 'user is a subscriber' do
+    let(:user) { User.create }
     it { is_expected.to forbid_new_and_create_actions }
   end
 
-  context 'user is a journalist' do
-    let(:user) { create(:user, role: 'journalist') }
+  context 'user of role subscriber cannot edit or update an article' do
+    let(:user) { User.create }
 
+    it { is_expected.to forbid_edit_and_update_actions }
+  end
+end
+
+describe ArticlePolicy do
+  subject { described_class.new(user, article) }
+  let(:user) { User.create(role: 'journalist') }
+  let(:article) { Article.create }
+
+  context 'user is a journalist' do
     it { is_expected.to permit_new_and_create_actions }
   end
 
   context 'journalist can edit & create an article' do
-    let(:user) { create(:user, role: 'journalist') }
-
-    it {is_expected.to permit_actions [:show, :index, :create, :edit] }
-  end
-
-  context 'user of role subscriber cannot edit or update an article' do
-    let(:user) {create(:user, role: 'subscriber')}
-
-    it { is_expected.to forbid_edit_and_update_actions }
+    it {is_expected.to permit_actions(%i[show index create edit]) }
   end
 end
