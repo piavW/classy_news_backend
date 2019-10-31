6e4238cffd93c2ffae64ea0cb0cf06c4993a0362
Yasmine Ezequiel <yasmine.ezequiel@gmail.com>
2019-10-31 16:13:51 +0100

    Eliminates comments from routes.

--- config/routes.rb
+++ config/routes.rb
@@ -3,12 +3,7 @@ Rails.application.routes.draw do
     namespace :v1 do
       resources :articles, only: [:index, :create, :update], constraints: { format: 'json' }
     end
-
-    namespace :v1 do
-      # mount_devise_token_auth_for 'User', at: 'auth', skip: [:omniauth_callbacks]
-    end
   end
 
   mount_devise_token_auth_for 'User', at: 'auth', skip: [:omniauth_callbacks]
-
 end
