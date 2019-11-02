11fe9c0b0c3fb2770f879e7fbe87f019f5deb410
piavw <piavonwachenfelt@hotmail.com>
2019-11-02 17:10:28 +0100

    Creates request-test for payment for subscription with stripe.

---
+++ spec/requests/api/v1/visitor_can_pay_for_subscription_spec.rb
@@ -0,0 +1,61 @@
+require 'stripe-mock'
+
+RSpec.describe Api::V1::SubscriptionsController, type: :request do
+  let(:stripe_helper) { StripeMock.create_test_helper }
+
+  before(:each) { StripeMock.start }
+  after(:each) { StripeMock.stop }
+  let(:new_subscriber) { create(:user, role: 'subscriber') }
+  let(:credentials) { new_subscriber.create_new_auth_token }
+  let(:headers) { { HTTP_ACCEPT: 'application/json' }.merge!(credentials) }
+
+  describe 'Payment posts successfully' do
+    before do
+      post '/api/v1/subscriptions', 
+      headers: headers, 
+      params: { stripeToken: StripeMock.generate_card_token }
+    end
+
+    it 'return status 200' do
+      expect(response.status).to eq 200
+    end
+
+    it 'returns success message' do
+      expect(response_json['message']).to eq 'Your payment was successful'
+    end
+  end
+
+  describe 'Payment is declined' do
+    describe 'because payment lacks stripe token' do
+      before do
+        post '/api/v1/subscriptions',
+        headers: headers,
+        params: { strikeToken: nil }
+      end
+
+      it 'returns status 402' do
+        expect(response.status).to eq 402
+      end
+
+      it 'returns error message' do
+        expect(response_json['errors']).to eq 'No Strike Token detected'
+      end
+    end
+
+    describe 'because payment had invalid stripe token' do
+      before do
+        post '/api/v1/subscriptions',
+        headers: headers,
+        params { stripeToken: 'invalid_token' }
+      end
+
+      it 'returns status 402' do
+        expect(response.status).to eq 402
+      end
+
+      it 'return error message' do
+        expect(response_json['errors']).to eq 'Invalid token id: id'   
+      end
+    end
+  end
+end
