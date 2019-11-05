e1770c8713db214d70b29c4aeb9b0966f5cf94da
Yasmine Ezequiel <yasmine.ezequiel@gmail.com>
2019-11-05 10:49:59 +0100

    Adds journalist role in seed db.

--- db/seeds.rb
+++ db/seeds.rb
@@ -3,5 +3,5 @@ Article.destroy_all
 user1 = User.create(email: 'user@mail.com', password: 'password', city: 'Stockholm', country: 'Sweden', role: 'subscriber')
 user2 = User.create(email: 'user2@mail.com', password: 'password', city: 'Stockholm', country: 'Sweden', role: 'journalist')
 user3 = User.create(email: 'user3@mail.com', password: 'password', city: 'Stockholm', country: 'Sweden', role: 'editor')
-article1 = Article.create(title: 'Swedens economy has never been better', content: 'Lorem impusm lalalala Trump', author: 'Pia Persson')
-article2 = Article.create(title: 'Brazil is building new rainforest', content: 'All of the content', author: 'Yasmine Persson')
+article1 = Article.create(title: 'Swedens economy has never been better', content: 'Lorem impusm lalalala Trump', author: 'Pia Persson', role: 'journalist')
+article2 = Article.create(title: 'Brazil is building new rainforest', content: 'All of the content', author: 'Yasmine Persson', role: 'journalist')
