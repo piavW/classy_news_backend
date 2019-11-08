0a0865489bc38fec062ed6a4067485a25858496d
piavw <piavonwachenfelt@hotmail.com>
2019-11-08 14:24:49 +0100

    Changes production storage to amazon, updates storage.yml to amazon, updates development config to amazon. Updates seed.

--- db/seeds.rb
+++ db/seeds.rb
@@ -1,7 +1,4 @@
 User.destroy_all
-Article.destroy_all
-user1 = User.create(email: 'user@mail.com', password: 'password', city: 'Stockholm', country: 'Sweden', role: 'subscriber')
-user2 = User.create(email: 'user2@mail.com', password: 'password', city: 'Stockholm', country: 'Sweden', role: 'journalist')
-user3 = User.create(email: 'user3@mail.com', password: 'password', city: 'Stockholm', country: 'Sweden', role: 'editor')
-article1 = Article.create(title: 'Swedens economy has never been better', content: 'Lorem impusm lalalala Trump', author: 'Pia Persson', journalist_id: 2)
-article2 = Article.create(title: 'Brazil is building new rainforest', content: 'All of the content', author: 'Yasmine Persson', journalist_id: 2)
+user1 = User.create(email: 'user@mail.com', password: 'password', name: 'John Doe', nickname: 'John', city: 'Stockholm', country: 'Sweden', role: 'subscriber')
+user1 = User.create(email: 'user1@mail.com', password: 'password', name: 'John Doe', nickname: 'John', city: 'Stockholm', country: 'Sweden', role: 'visitor')
+user2 = User.create(email: 'user2@mail.com', password: 'password', name: 'Molly Weasley', nickname: 'Molly', city: 'Stockholm', country: 'Sweden', role: 'journalist')
