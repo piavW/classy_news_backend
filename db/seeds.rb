82ce18bd3f808ac7bd85e094d3243799b4e15afb
piavw <piavonwachenfelt@hotmail.com>
2019-11-08 14:38:13 +0100

    Changes seed to not have the same credentials

--- db/seeds.rb
+++ db/seeds.rb
@@ -1,4 +1,4 @@
 User.destroy_all
 user1 = User.create(email: 'user@mail.com', password: 'password', name: 'John Doe', nickname: 'John', city: 'Stockholm', country: 'Sweden', role: 'subscriber')
-user1 = User.create(email: 'user1@mail.com', password: 'password', name: 'John Doe', nickname: 'John', city: 'Stockholm', country: 'Sweden', role: 'visitor')
+user1 = User.create(email: 'user1@mail.com', password: 'password', name: 'David Doe', nickname: 'Davve', city: 'Stockholm', country: 'Sweden', role: 'visitor')
 user2 = User.create(email: 'user2@mail.com', password: 'password', name: 'Molly Weasley', nickname: 'Molly', city: 'Stockholm', country: 'Sweden', role: 'journalist')
